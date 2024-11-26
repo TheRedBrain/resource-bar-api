5ba59df8f25851f6540640eeff8075d51fdf8eeb
TheRedBrain <thetrueredbrain@gmail.com>
2024-11-26 21:22:26 +0100

    added test mod

---
+++ src/testmod/java/com/github/theredbrain/resourcebarapi_test/ResourceBarAPITest.java
@@ -0,0 +1,20 @@
+package com.github.theredbrain.resourcebarapi_test;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourceBarAPITest implements ModInitializer {
+	public static final String MOD_ID = "resourcebarapi_test";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Testing Resource Bar API!");
+	}
+
+	public static Identifier identifier(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
---
+++ src/testmod/java/com/github/theredbrain/resourcebarapi_test/config/ClientConfig.java
@@ -0,0 +1,175 @@
+package com.github.theredbrain.resourcebarapi_test.config;
+
+import com.github.theredbrain.resourcebarapi.ResourceBarAPI;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(
+		name = "client"
+)
+public class ClientConfig implements ConfigData {
+	public boolean show_resource_bar = true;
+	public boolean show_full_resource_bar = true;
+
+	@Comment("current_value")
+	public int current_value = 5;
+	@Comment("max_value")
+	public int max_value = 10;
+	@Comment("current_value_reduction")
+	public int current_value_reduction = 0;
+	@Comment("current_value_reservation")
+	public int current_value_reservation = 2;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("is_centered")
+	public boolean is_centered = false;
+
+	@Comment("offset_x")
+	public int offset_x = -91;
+	@Comment("offset_y")
+	public int offset_y = -45;
+	@ConfigEntry.Gui.EnumHandler(
+			option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON
+	)
+	@Comment("fill_direction")
+	public ResourceBarAPI.ResourceBarFillDirection fill_direction = ResourceBarAPI.ResourceBarFillDirection.LEFT_TO_RIGHT;
+	@ConfigEntry.Gui.EnumHandler(
+			option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON
+	)
+	@Comment("origin")
+	public ResourceBarAPI.ResourceBarOrigin origin = ResourceBarAPI.ResourceBarOrigin.BOTTOM_MIDDLE;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("background_middle_segment_amount")
+	public int background_middle_segment_amount = 172;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("horizontal_background_left_end_width")
+	public int horizontal_background_left_end_width = 5;
+	@Comment("horizontal_background_middle_segment_width")
+	public int horizontal_background_middle_segment_width = 1;
+	@Comment("horizontal_background_right_end_width")
+	public int horizontal_background_right_end_width = 5;
+	@Comment("horizontal_background_height")
+	public int horizontal_background_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_background_width")
+	public int vertical_background_width = 5;
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_background_top_end_height")
+	public int vertical_background_top_end_height = 5;
+	@Comment("vertical_background_middle_segment_height")
+	public int vertical_background_middle_segment_height = 1;
+	@Comment("vertical_background_bottom_end_height")
+	public int vertical_background_bottom_end_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("progress_offset_x")
+	public int progress_offset_x = 0;
+	@Comment("progress_offset_y")
+	public int progress_offset_y = 0;
+	@Comment("progress_middle_segment_amount")
+	public int progress_middle_segment_amount = 172;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("horizontal_progress_left_end_width")
+	public int horizontal_progress_left_end_width = 5;
+	@Comment("horizontal_progress_middle_segment_width")
+	public int horizontal_progress_middle_segment_width = 1;
+	@Comment("horizontal_progress_right_end_width")
+	public int horizontal_progress_right_end_width = 5;
+	@Comment("horizontal_progress_height")
+	public int horizontal_progress_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_progress_width")
+	public int vertical_progress_width = 5;
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_progress_top_end_height")
+	public int vertical_progress_top_end_height = 5;
+	@Comment("vertical_progress_middle_segment_height")
+	public int vertical_progress_middle_segment_height = 1;
+	@Comment("vertical_progress_bottom_end_height")
+	public int vertical_progress_bottom_end_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("reserved_offset_x")
+	public int reserved_offset_x = 0;
+	@Comment("reserved_offset_y")
+	public int reserved_offset_y = 0;
+	@Comment("reserved_middle_segment_amount")
+	public int reserved_middle_segment_amount = 172;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("horizontal_reserved_left_end_width")
+	public int horizontal_reserved_left_end_width = 5;
+	@Comment("horizontal_reserved_middle_segment_width")
+	public int horizontal_reserved_middle_segment_width = 1;
+	@Comment("horizontal_reserved_right_end_width")
+	public int horizontal_reserved_right_end_width = 5;
+	@Comment("horizontal_reserved_height")
+	public int horizontal_reserved_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_reserved_width")
+	public int vertical_reserved_width = 5;
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_reserved_top_end_height")
+	public int vertical_reserved_top_end_height = 5;
+	@Comment("vertical_reserved_middle_segment_height")
+	public int vertical_reserved_middle_segment_height = 1;
+	@Comment("vertical_reserved_bottom_end_height")
+	public int vertical_reserved_bottom_end_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("show_current_value_overlay")
+	public boolean show_current_value_overlay = false;
+
+	@Comment("overlay_offset_x")
+	public int overlay_offset_x = -2;
+	@Comment("overlay_offset_y")
+	public int overlay_offset_y = 0;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("horizontal_overlay_width")
+	public int horizontal_overlay_width = 5;
+	@Comment("horizontal_overlay_height")
+	public int horizontal_overlay_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("vertical_overlay_width")
+	public int vertical_overlay_width = 5;
+	@Comment("vertical_overlay_height")
+	public int vertical_overlay_height = 5;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("enable_smooth_animation")
+	public boolean enable_smooth_animation = true;
+	@Comment("animation_interval")
+	public int animation_interval = 1;
+	@Comment("max_value_change_is_animated")
+	public boolean max_value_change_is_animated = false;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("show_number")
+	public boolean show_number = true;
+
+	@ConfigEntry.Gui.PrefixText
+	@Comment("show_max_value")
+	public boolean show_max_value = false;
+
+	@Comment("number_offset_x")
+	public int number_offset_x = 0;
+	@Comment("number_offset_y")
+	public int number_offset_y = -46;
+
+	@Comment("number_color")
+	public int number_color = -6250336;
+
+
+	public ClientConfig() {
+	}
+}
